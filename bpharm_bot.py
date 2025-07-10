c59a2205ecfd399a19e20696f207fda613508fd2
codeCrafter02 <minecraftaspirants@gmail.com>
2025-07-10 15:54:30 +0530

    Update bpharm_bot.py

--- bpharm_bot.py
+++ bpharm_bot.py
@@ -124,3 +124,6 @@ async def webhook():
 @app.route("/")
 def home():
     return "Bot is running!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
